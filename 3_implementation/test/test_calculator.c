8d147320d56e7ff5fe302fdbcb7c84db9bc7687a
visweswar299 <80445066+visweswar299@users.noreply.github.com>
2021-04-15 02:16:09 -0700

    Add files via upload

---
+++ 3_implementation/test/test_calculator.c
@@ -0,0 +1,122 @@
+#include "unity.h"
+#include "unity_internals.h"
+#include "main.h"
+#include<math.h>
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+void test_factorial(void)
+{
+  TEST_ASSERT_EQUAL(120, Factorial(5));
+  TEST_ASSERT_EQUAL(720, Factorial(6));
+}
+void test_factorial_zero(void)
+{
+  TEST_ASSERT_EQUAL(1, Factorial(0));
+  TEST_ASSERT_EQUAL(1, Factorial(1));
+}
+void test_factorial_Negative(void)
+{
+  TEST_ASSERT_EQUAL(-1, Factorial(-1));
+  TEST_ASSERT_EQUAL(-1, Factorial(-10));
+}
+void test_zero_one(void)
+{
+  TEST_ASSERT_EQUAL(1, Factorial(0));
+  TEST_ASSERT_EQUAL(1, Factorial(1));
+}
+void test_negative(void)
+{
+  TEST_ASSERT_EQUAL(-1, Factorial(-5));
+  TEST_ASSERT_EQUAL(-1, Factorial(-10));
+}
+void test_Addition(void)
+{
+  TEST_ASSERT_EQUAL(10, Addition(5,5));
+  TEST_ASSERT_EQUAL(300, Addition(100,200));
+}
+void test_Subtraction(void)
+{
+  TEST_ASSERT_EQUAL(0, Subtraction(5,5));
+  TEST_ASSERT_EQUAL(100, Subtraction(200,100));
+}
+void test_Multiplication(void)
+{
+  TEST_ASSERT_EQUAL(25, Multiplication(5,5));
+  TEST_ASSERT_EQUAL(1000, Multiplication(10,100));
+}
+void test_Multiplication_zero(void)
+{
+  TEST_ASSERT_EQUAL(0, Multiplication(5,0));
+  TEST_ASSERT_EQUAL(0, Multiplication(0,100));
+}
+void test_Multiplication_negative(void)
+{
+  TEST_ASSERT_EQUAL(-1, Multiplication(-5,1));
+  TEST_ASSERT_EQUAL(-1, Multiplication(1,-10));
+}
+void test_Division_zero(void)
+{
+  TEST_ASSERT_EQUAL(-1, Division(5,0));
+  TEST_ASSERT_EQUAL(-1, Division(10,0));
+}
+void test_Division_Negative(void)
+{
+  TEST_ASSERT_EQUAL(-1, Division(5,-1));
+  TEST_ASSERT_EQUAL(-1, Division(100,-1));
+}
+void test_Division(void)
+{
+  TEST_ASSERT_EQUAL(5, Division(25,5));
+  TEST_ASSERT_EQUAL(10, Division(100,10));
+}
+void test_Power(void)
+{
+  //TEST_ASSERT_EQUAL(8, Power(2,3));
+  TEST_ASSERT_EQUAL(1000, Power(10,3));
+}
+void test_power_Zero(void)
+{
+  TEST_ASSERT_EQUAL(1, Power(2,0));
+  TEST_ASSERT_EQUAL(1, Power(10,0));
+}
+void test_Modulus_Zero(void)
+{
+  TEST_ASSERT_EQUAL(-1, Modulus(5,0));
+  TEST_ASSERT_EQUAL(-1, Modulus(10,0));
+}
+void test_Modulus(void)
+{
+  TEST_ASSERT_EQUAL(1, Modulus(2,2));
+  TEST_ASSERT_EQUAL(1, Power(10,3));
+}
+int main(void)
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_factorial);
+  RUN_TEST(test_factorial_zero);
+  RUN_TEST(test_factorial_Negative);
+  RUN_TEST(test_zero_one);
+  RUN_TEST(test_negative);
+  RUN_TEST(test_Addition);
+  RUN_TEST(test_Subtraction);
+  RUN_TEST(test_Multiplication);
+  RUN_TEST(test_Multiplication_zero);
+  RUN_TEST(test_Multiplication_negative);
+  RUN_TEST(test_Division_zero);
+  RUN_TEST(test_Division_Negative);
+  RUN_TEST(test_Division);
+  RUN_TEST(test_Power);
+  RUN_TEST(test_power_Zero);
+  RUN_TEST(test_Modulus_Zero);
+  RUN_TEST(test_Modulus);
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
