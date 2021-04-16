01b6f8405e3289b823d3af7905212766fcccd243
visweswar299 <80445066+visweswar299@users.noreply.github.com>
2021-04-16 02:51:40 -0700

    Update Power.c

--- 3_implementation/src/Power.c
+++ 3_implementation/src/Power.c
@@ -1,5 +1,16 @@
 #include "calculator.h"
 
+
+
+
+
+/**
+ * @brief  function to find the Power of the number
+ * 
+ * @param a operand1
+ * @param b operand2
+ * @return int  it returns the output value
+ */
 int Power(int a ,int b)
 {
     int (*power)(int,int)=&Power;
