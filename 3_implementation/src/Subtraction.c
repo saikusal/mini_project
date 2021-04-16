439cf1ff0c31872f956808621abe4af691db28c9
visweswar299 <80445066+visweswar299@users.noreply.github.com>
2021-04-16 02:52:58 -0700

    Update Subtraction.c

--- 3_implementation/src/Subtraction.c
+++ 3_implementation/src/Subtraction.c
@@ -1,6 +1,15 @@
 #include "calculator.h"
 
 
+
+
+/**
+ * @brief  function to subtraction of two numbers
+ * 
+ * @param a operand1
+ * @param b operand2
+ * @return int  it returns the output value
+ */
  int (*minus)(int,int)=&Subtraction;
  int Subtraction(int a ,int b)
 {
