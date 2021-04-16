3f1fd736169afd4b6001ab735d1fcc2536f9711f
visweswar299 <80445066+visweswar299@users.noreply.github.com>
2021-04-16 02:48:17 -0700

    Update Addition.c

--- 3_implementation/src/Addition.c
+++ 3_implementation/src/Addition.c
@@ -1,5 +1,13 @@
-#include  "main.h"
+#include  "calculator.h"
 
+
+/**
+ * @brief  function to addition of two numbers
+ * 
+ * @param a operand1
+ * @param b operand2
+ * @return int  it returns the output value
+ */
 int Addition(int a, int b)
 {
     int (*add)(int , int)= &Addition;
