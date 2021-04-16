a0c725ce4e3feda6087125d57ad56744ff568e5b
visweswar299 <80445066+visweswar299@users.noreply.github.com>
2021-04-16 02:52:28 -0700

    Update Square.c

--- 3_implementation/src/Square.c
+++ 3_implementation/src/Square.c
@@ -1,5 +1,15 @@
 #include "calculator.h"
 
+
+
+
+
+/**
+ * @brief  function to find squares of a number
+ * 
+ * @param a operand1
+ * @return int  it returns the output value
+ */
 int Square(int a)
 {
     int (*square)(int)=&Square;
